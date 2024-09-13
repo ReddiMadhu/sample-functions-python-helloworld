4ae3fb68362316e9d3fff2c876649e9e5fde0641
ReddiMadhu <83718786+ReddiMadhu@users.noreply.github.com>
2024-09-13 06:55:15 +0530

    Update hello.py

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,7 +1,13 @@
 def main(args):
-      name = args.get("name", "stranger")
-      user_from=args.get("from")
-      user_to=args.get("to")
-      
-      return {"body": greeting,"name":user_from,"to":user_to}
-  
+    name = args.get("name", "stranger")
+    user_from = args.get("from")
+    user_to = args.get("to")
+    
+    # Constructing the greeting string using the inputs
+    if user_from and user_to:
+        greeting = f"Hello {name}, from {user_from} to {user_to}!"
+    else:
+        greeting = f"Hello {name}!"
+    
+    return {"body": greeting}
+
